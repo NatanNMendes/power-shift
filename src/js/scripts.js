71720752c1f84ab98782d421e9c32d1a7af94089
NatanNMendes <natan_nunes_mendes@outlook.com>
2025-03-05 11:06:45 -0300

    codigo ajustado

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,15 @@
-// Captura o botão de menu e o menu móvel
-const menuButton = document.getElementById('mobile-menu-button');
-const mobileMenu = document.getElementById('mobile-menu');
-
-// Adiciona o evento de clique para alternar o menu
-menuButton.addEventListener('click', () => {
-  // Alterna a classe 'hidden' no menu
-  mobileMenu.classList.toggle('hidden');
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.querySelector("button[aria-controls='mobile-menu']");
+    const mobileMenu = document.getElementById("mobile-menu");
+    const menuIcon = menuButton.querySelector("svg:first-of-type");
+    const closeIcon = menuButton.querySelector("svg:last-of-type");
+    closeIcon.classList.add("hidden");
+    
+    menuButton.addEventListener("click", function () {
+        const isExpanded = menuButton.getAttribute("aria-expanded") === "true";
+        menuButton.setAttribute("aria-expanded", !isExpanded);
+        mobileMenu.classList.toggle("hidden");
+        menuIcon.classList.toggle("hidden", !isExpanded);
+        closeIcon.classList.toggle("hidden", isExpanded);
+    });
 });
